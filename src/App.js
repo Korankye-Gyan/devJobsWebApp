32bde7cf153c07c0daeec63b9b8cb630d5f0a24d
Korankye-Gyan <korankyefoster@gmail.com>
2023-06-06 01:15:45 +0000

    DarkMode on FilterAlt partially implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ThemeContext from './ThemeContext';
 import Header from './components/Header';
@@ -6,38 +6,38 @@ import Jobdetails from './pages/Jobdetails';
 import Joblist from './pages/Joblist';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-//import {red,blue} from '@mui/material/colors'
-
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-  
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   const theme = createTheme({
     palette: {
-      mode: isDarkMode ? 'dark':'light',
-      //primary: isDarkMode ? red : blue,
+      mode: isDarkMode ? 'dark' : 'light',
+      background: {
+        default: isDarkMode ? '#000000' : '#F4F6F8',
+      },
     },
   });
 
   const toggleDarkMode = () => {
     setIsDarkMode((prevDarkMode) => !prevDarkMode);
-   };
+  };
 
   return (
-   <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode}}>
-   <ThemeProvider theme={theme}>
-      <CssBaseline />
-    <div className="App">
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Joblist />} />
-            <Route path="/jobDetails/:position" element={<Jobdetails />} />
-          </Routes>
-        </Router>
-     </div>
-   </ThemeProvider>
-   </ThemeContext.Provider>
+    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Joblist />} />
+              <Route path="/jobDetails/:position" element={<Jobdetails />} />
+            </Routes>
+          </Router>
+        </div>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
 
--- src/pages/Joblist.js
+++ src/pages/Joblist.js
@@ -5,7 +5,11 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Button from "../components/Buttons";
 import Job from '../components/Job';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
+  
+  
 
 const Joblist = () => {
   const [visibleCards, setVisibleCards] = useState(6); //Number of initial visible cards
@@ -14,6 +18,9 @@ const Joblist = () => {
   const [locationFilter, setLocationFilter] = useState('');
   const [fullTimeOnly, setFullTimeOnly] = useState(false);
   const [isFilterPromptOpen, setIsFilterPromptOpen] = useState(false);
+
+  const theme = useTheme();
+  const isDarkMode = useMediaQuery(theme.breakpoints.down('md'));
 
   useEffect(() => {
     fetch("/data.json") // fetch the JSON data from the public URL path
@@ -169,7 +176,7 @@ const Joblist = () => {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            backgroundColor: 'white',
+            backgroundColor: isDarkMode ? 'dark' : '#ffffff',
             padding: '20px',
             borderRadius: '8px',
             boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.15)',
